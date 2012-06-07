535ee6eea11cf78bda32d5089765f97ac01d17a3
unknown <hibiki131@.(none)>
2012-06-07 16:54:04 +0900

    fix to object oriented

--- public_html/js/canvaswater.js
+++ public_html/js/canvaswater.js
@@ -10,45 +10,97 @@
 * @url http://github.com/yuukimiyo/CanvasWater
 * @github http://github.com/yuukimiyo/CanvasWater
 *
-*/ 
+*/
 
-window.onload=function(){
-	fitCanvas();
-	draw();
-	
-	$(window).resize(function() {
-		fitCanvas();
-		draw();
-	}); 
+var cm;
+
+/**
+ * constractor 
+ */
+function canvaswater() {
+	cm = new CanvasManager(); 
 }
 
-function fitCanvas() {
+/**
+ * program loader. 
+ */
+window.onload=function(){
 	if (!document.getElementById('canvaswater')) { return false; }
 	if (!document.getElementById('wrapper')) { return false; }
 	
-	$("#canvaswater").attr({height:$("#wrapper").height()});
-	$("#canvaswater").attr({width:$("#wrapper").width()});
-}
-
-function draw() {
-	if (!document.getElementById('canvaswater')) { return false; }
 	var canvas = document.getElementById('canvaswater');
 	if (!canvas.getContext) { return false; }
-  
-	var ctx = canvas.getContext('2d');
-
-	ctx.fillStyle = 'rgb(0, 80, 77)';
-	ctx.fillRect(0, 0, $("#wrapper").width(), $("#wrapper").height());
-	ctx.fill();
-
-	ctx.beginPath();
-	ctx.moveTo(20, 20);
-	ctx.lineTo(200, 20);
-	ctx.lineTo(200, 200);
-	ctx.lineTo(20, 200);
-	ctx.closePath();
-	ctx.fillStyle = 'rgb(192, 80, 77)';
-	ctx.fill();
 	
-	ctx.stroke();
+	cm.ctx = canvas.getContext('2d');
+	cm.offsetLeft = $("#canvaswater").offset()["left"];
+	cm.offsetTop = $("#canvaswater").offset()["top"];
+	
+	cm.resizeCanvas($("#wrapper").width(),$("#wrapper").height());
+	cm.drawBackground();
+	
+	$(window).resize(function() {
+		cm.resizeCanvas($("#wrapper").width(),$("#wrapper").height());
+		cm.drawBackground();
+	});
+	
+	$("#canvaswater").mousedown(function(e){
+		cm.clickHandra(e);
+    });
+    
+    $("#canvaswater").mouseup(function(e){
+		cm.deleteCircle(e);
+    });
 }
+
+function CanvasManager(){
+	var obj = this;
+	
+	obj.ctx;
+	obj.mouseX = 0;
+	obj.mouseY = 0;
+	
+	obj.offsetLeft = 0;
+	obj.offsetTop = 0;
+	obj.canvasWidth = 0;
+	obj.canvasHeight = 0;
+	
+	obj.bgColor = 'rgb(0, 80, 77)';
+	obj.rectColor = 'rgb(192, 80, 77)';
+	
+	obj.startRadius = 40;
+	obj.radius = obj.startRadius;
+	
+	obj.drawBackground = function() {
+		obj.ctx.fillStyle = obj.bgColor;
+		obj.ctx.fillRect(obj.offsetLeft, obj.offsetTop, obj.canvasWidth, obj.canvasHeight);
+		obj.ctx.fill();
+	}
+	
+	obj.clickHandra = function(e) {
+		obj.mouseX = Math.floor(e.pageX - obj.offsetLeft);
+		obj.mouseY = Math.floor(e.pageY - obj.offsetTop);
+		obj.drawCircle();
+	}
+	
+	obj.drawCircle = function() {
+		obj.ctx.beginPath();
+		obj.ctx.arc(obj.mouseX, obj.mouseY, obj.radius, 0, Math.PI*2, false);
+		obj.ctx.stroke();
+	}
+	
+	obj.deleteCircle = function() {
+		obj.ctx.beginPath();
+		obj.ctx.clearRect(obj.offsetLeft, obj.offsetTop, obj.canvasWidth, obj.canvasHeight);
+		obj.drawBackground();
+	}
+	
+	obj.resizeCanvas = function(w,h) {
+		obj.canvasWidth = w;
+		obj.canvasHeight = h;
+		
+		$("#canvaswater").attr({width:obj.canvasWidth});
+		$("#canvaswater").attr({height:obj.canvasHeight});
+	}
+}
+
+canvaswater();
